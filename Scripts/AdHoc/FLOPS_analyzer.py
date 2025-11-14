c453a5499d39afc3b032114ec0764250461a813b
AryanGarg13 <aryan.garg@students.iiit.ac.in>
2025-11-14 19:46:24 +0530

    Rotation Stuff

--- Scripts/AdHoc/FLOPS_analyzer.py
+++ Scripts/AdHoc/FLOPS_analyzer.py
@@ -2,17 +2,18 @@ import torch
 from fvcore.nn import FlopCountAnalysis, flop_count_table
 from pathlib import Path
 
-from Module.Frontend.Frontend import IFrontend, ModelContext
+from Module.Frontend.Frontend import IFrontend#, ModelContext
 from Module.Frontend.Matching import IMatcher
 from Module.Frontend.StereoDepth import IStereoDepth
 from Utility.Config import load_config
 
 
-def GetFlops(model: IFrontend[ModelContext] | IMatcher[ModelContext] | IStereoDepth[ModelContext]):
-    input_A = torch.rand(2, 3, 640, 640, device='cuda')
-    input_B = torch.rand(2, 3, 640, 640, device='cuda')
-        
-    flops = FlopCountAnalysis(model.context["model"], (input_A, input_B))
+# def GetFlops(model: IFrontend[ModelContext] | IMatcher[ModelContext] | IStereoDepth[ModelContext]):
+def GetFlops(model):
+    input_A = torch.rand(2, 3, 640, 640, device='cpu')
+    input_B = torch.rand(2, 3, 640, 640, device='cpu')
+    print(type(model.model))
+    flops = FlopCountAnalysis(model.model, (input_A, input_B))
     print(f"Total Flops: {flops.total()/1e9} G")
     print(flop_count_table(flops))
 
@@ -20,14 +21,14 @@ def GetFlops(model: IFrontend[ModelContext] | IMatcher[ModelContext] | IStereoDe
 def main(cfg_path: str, module_type: str):
     cfg, _ = load_config(Path(cfg_path))
     
-    module: IMatcher[ModelContext] | IStereoDepth[ModelContext] | IFrontend[ModelContext]
+    # module: IMatcher[ModelContext] | IStereoDepth[ModelContext] | IFrontend[ModelContext]
     match module_type:
         case "frontend":
-            module = IFrontend.instantiate(cfg.type, cfg.args)
+            module = IFrontend.instantiate(cfg.Odometry.frontend.type, cfg.Odometry.frontend.args)
         case "depth":
-            module = IStereoDepth.instantiate(cfg.type, cfg.args)
+            module = IStereoDepth.instantiate(cfg.depth.type, cfg.args)
         case "flow":
-            module = IMatcher.instantiate(cfg.type, cfg.args)
+            module = IMatcher.instantiate(cfg.flow.type, cfg.args)
         case _:
             raise ValueError(f"module_type Must be one of 'frontend', 'depth' or 'flow'. Get {_}")
     
